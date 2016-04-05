a980f399524c76df347c9b8b777f78affeb8792e
Luke Preston <luke.preston@bbc.co.uk>
2016-04-05 09:58:42 +0100

    updating you eye

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
+for port in $(seq 4444 65000); do echo -ne "\035" | telnet 127.0.0.1 $port > /dev/null 2>&1; [ $? -eq 1 ] && break; done
+
 if [ -e jekyll.pid ]
 then
     kill -9 `cat jekyll.pid`
@@ -7,7 +9,7 @@ fi
 clear
 
 export JEKYLL_ENV=dev
-bundle exec jekyll serve --watch &
+bundle exec jekyll serve --watch --port $port &
 
 echo $! > jekyll.pid
 echo "Started: " `date`
