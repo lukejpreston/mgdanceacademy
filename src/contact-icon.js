4a145159473ddd0f12de70e2a04babd91d96074d
luke <lukejpreston@gmail.com>
2018-06-15 22:00:15 +0100

    adding new icons

---
+++ src/contact-icon.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default () => <svg width='400' height='400' viewBox='0 0 400 400' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <path d='M200 400C310.457 400 400 310.457 400 200C400 89.5431 310.457 0 200 0C89.5431 0 0 89.5431 0 200C0 310.457 89.5431 400 200 400Z' fill='#6CA6F3' />
+  <g style='mix-blend-mode:overlay'>
+    <path style='mix-blend-mode:overlay' fillRule='evenodd' clipRule='evenodd' d='M192.121 266.74C263.226 236.08 313 165.353 313 83C313 74.9257 312.522 66.9631 311.591 59.1392L227 0L0 156.5L192.121 266.74Z' transform='translate(87 117)' fill='url(#paint0_radial)' />
+  </g>
+  <path d='M221 0H7C3.13401 0 0 3.13401 0 7V150C0 153.866 3.13401 157 7 157H221C224.866 157 228 153.866 228 150V7C228 3.13401 224.866 0 221 0Z' transform='translate(86 116)' fill='#EA3737' />
+  <g style='mix-blend-mode:overlay'>
+    <path style='mix-blend-mode:overlay' d='M118 0V139.352C118 145.054 111.547 148.364 106.916 145.036L0 68.223L118 0Z' transform='translate(196 124)' fill='url(#paint1_radial)' />
+  </g>
+  <mask id='mask0' mask-type='alpha' maskUnits='userSpaceOnUse' x='86' y='116' width='229' height='77'>
+    <path d='M221.05 0C228.198 0 230.749 9.4535 224.571 13.0497L116.545 75.9307C114.35 77.2081 111.636 77.197 109.452 75.9016L3.44089 13.0206C-2.68131 9.38914 -0.106114 0 7.01199 0H221.05Z' transform='translate(86.0104 116)' fill='white' />
+  </mask>
+  <g mask='url(#mask0)'>
+    <path d='M221.05 0C228.198 0 230.749 9.4535 224.571 13.0497L116.545 75.9307C114.35 77.2081 111.636 77.197 109.452 75.9016L3.44089 13.0206C-2.68131 9.38914 -0.106114 0 7.01199 0H221.05Z' transform='translate(86.0104 116)' stroke='white' strokeWidth='8' />
+  </g>
+  <path fillRule='evenodd' clipRule='evenodd' d='M4 148.004C2.0484 147.164 0.555237 145.462 0 143.377V0H4V148.004Z' transform='translate(86 124)' fill='white' />
+  <path fillRule='evenodd' clipRule='evenodd' d='M0 143.377V0H4V148.004C2.0484 147.164 0.555206 145.462 0 143.377Z' transform='translate(314 124) scale(-1 1)' fill='white' />
+  <defs>
+    <radialGradient id='paint0_radial' cx='0.5' cy='0.5' r='0.5' gradientUnits='userSpaceOnUse' gradientTransform='matrix(807 478 -428.004 722.593 -102.498 -555.796)'>
+      <stop offset='0.389448' stopColor='#213D62' />
+      <stop offset='0.540955' stopColor='#291D1D' stopOpacity='0.2' />
+    </radialGradient>
+    <radialGradient id='paint1_radial' cx='0.5' cy='0.5' r='0.5' gradientUnits='userSpaceOnUse' gradientTransform='matrix(273.316 157.515 -164.449 282.374 -12.5791 -177.305)'>
+      <stop offset='0.38674' stopColor='#120F0F' />
+      <stop offset='0.815996' stopColor='#291D1D' stopOpacity='0.2' />
+    </radialGradient>
+  </defs>
+</svg>
