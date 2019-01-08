a84c53bd162d551a1575ec1392c010afa4cc83c1
luke <lukejpreston@gmail.com>
2019-01-08 10:55:47 +0000

    Updates

---
+++ precache-manifest.02c63435b1073f189d21cce4c9f30eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e25f92eb7dd35d737b8",
+    "url": "/static/js/main.2e25f92e.chunk.js"
+  },
+  {
+    "revision": "057d19551102191bb705",
+    "url": "/static/js/1.057d1955.chunk.js"
+  },
+  {
+    "revision": "2e25f92eb7dd35d737b8",
+    "url": "/static/css/main.fe82570f.chunk.css"
+  },
+  {
+    "revision": "46e093202bcb9662834b2a8a9a9f4582",
+    "url": "/index.html"
+  }
+];
